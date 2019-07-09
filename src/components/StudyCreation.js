e34176aaa4bcbff4ba8818743f4d4f53b837a2a1
Ludwig Dickmanns <dickmanns@fortiss.org>
2019-07-09 15:02:54 +0200

    StudyCreation:
    * Added methods for handling change, added state, adjusted forms

--- src/components/StudyCreation.js
+++ src/components/StudyCreation.js
@@ -3,53 +3,203 @@
 import React from 'react';
 
 import Page from './Page'
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-export const StudyCreation = () => (
-    <Page>
-        <h1>Create new study</h1>
-        <Form>
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
+export class StudyCreation extends React.Component {
 
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Description</Form.Label>
-                <Form.Control as="textarea" rows="5" />
-            </Form.Group>
+    constructor(props) {
+        super(props);
 
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Participants</Form.Label>
-                <Form.Control type="text" placeholder="Enter participants" />
-            </Form.Group>
+        this.state = {
+            title: '',
+            description: '',
+            prerequisites: '',
+            capacity: '',
+            country: '',
+            city: '',
+            zip: '',
+            street: '',
+            number: '',
+            additionalLocationInfo: '',
+            rewardCurrency: '',
+            rewardAmount: '',
+            rewardType: '',
+        };
 
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Time schedule</Form.Label>
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handleCapacityChange = this.handleCapacityChange.bind(this);
 
-            </Form.Group>
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Tags</Form.Label>
-                <Form.Control type="text" placeholder="Enter tags" />
-            </Form.Group>
+    handleTitleChange(event) {
+        console.log(event.target.name)
+        this.setState({
+            title: event.target.value
+        });
+    }
 
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Reward (per participant)</Form.Label>
-                <Form.Control type="text" placeholder="Enter reward per participant" />
-            </Form.Group>
+    handleDescriptionChange(event) {
+        this.setState({
+            description: event.target.value
+        });
+    }
 
-            <Form.Group controlId="exampleForm.ControlSelect1">
-                <Form.Label>Payment method</Form.Label>
-                <Form.Control as="select">
-                    <option>Paypal</option>
-                </Form.Control>
-            </Form.Group>
+    handlePrerequisitesChange(event) {
+        this.setState({
+            prerequisites: event.target.value
+        });
+    }
 
-            <Button variant="primary" type="submit">
-                Create
-            </Button>
-        </Form>
-    </Page>
-);
+    handleCapacityChange(event) {
+        this.setState({
+            capacity: event.target.value
+        });
+    }
+
+    handleCountryChange(event) {
+        this.setState({
+            country: event.target.value
+        });
+    }
+
+    handleCityChange(event) {
+        this.setState({
+            city: event.target.value
+        });
+    }
+
+    handleZipChange(event) {
+        this.setState({
+            zip: event.target.value
+        });
+    }
+
+    handleStreetChange(event) {
+        this.setState({
+            street: event.target.value
+        });
+    }
+
+    handleNumberChange(event) {
+        this.setState({
+            number: event.target.value
+        });
+    }
+
+    handleAddLocInfoChange(event) {
+        this.setState({
+            additionalLocationInfo: event.target.value
+        });
+    }
+
+    handleRewardCurrencyChange(event) {
+        this.setState({
+            rewardCurrency: event.target.value
+        });
+    }
+
+    handleRewardAmountChange(event) {
+        this.setState({
+            rewardAmount: event.target.value
+        });
+    }
+
+    handleRewardTypeChange(event) {
+        this.setState({
+            rewardType: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        let study = {
+            title: this.state.title,
+            description: this.state.description,
+            prerequisites: this.state.prerequisites,
+            capacity: this.state.capacity,
+            country: this.state.country,
+            city: this.state.city,
+            zip: this.state.zip,
+            street: this.state.street,
+            number: this.state.number,
+            additionalLocationInfo: this.state.additionalLocationInfo,
+            rewardCurrency: this.state.rewardCurrency,
+            rewardAmount: this.state.rewardAmount,
+            rewardType: this.state.rewardType,
+        }
+
+        // TODO do submit
+    }
+
+    render() {
+        return(
+            <Page>
+                <h1>Create new study</h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            name='title'
+                            type='text'
+                            placeholder='Enter study title'
+                            value={this.state.title}
+                            onChange={this.handleTitleChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            name='description'
+                            as='textarea'
+                            rows='5'
+                            value={this.state.description}
+                            onChange={this.handleDescriptionChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Participant Capacity</Form.Label>
+                        <Form.Control 
+                            name='capacity'
+                            type='text' 
+                            placeholder='Enter participants'
+                            value={this.state.capacity}
+                            onChange={this.handleCapacityChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Time schedule</Form.Label>
+                        <Form.Control id='studyTimeSchedule'/>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Tags</Form.Label>
+                        <Form.Control type='text' placeholder='Enter tags' />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Reward (per participant)</Form.Label>
+                        <Form.Control type='text' placeholder='Enter reward per participant' />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Payment method</Form.Label>
+                        <Form.Control id='studyPaymentMethod' as='select'>
+                            <option>PayPal</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Button variant='primary' type='submit'>
+                        Create
+                    </Button>
+                </Form>
+            </Page>
+        );
+    }
+}
