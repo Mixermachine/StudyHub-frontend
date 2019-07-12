c3b0af13b6aed744f31965ced4f69841011cea32
Nico Schreder <nschreder@w211-3k-v4.eduroam.dynamic.rbg.tum.de>
2019-07-12 16:17:25 +0200

    Implementation of authentication and registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,63 +1,158 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom'
 
 import Page from './Page'
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import {AlertMessage} from "./movie/AlertMessage";
 
+export class Register extends React.Component {
 
-export const Register = () => (
-    <Page>
-        <h1>Register</h1>
-        <Form>
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Firstname</Form.Label>
-                <Form.Control type="text" placeholder="Enter firstname" />
-            </Form.Group>
+    constructor(props) {
+        super(props);
 
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Lastname</Form.Label>
-                <Form.Control type="text" placeholder="Enter lastname" />
-            </Form.Group>
+        this.state = {
+            firstName: '',
+            lastName: '',
+            DoB: '',
+            gender: '',
+            email: '',
+            password: '',
+            passwordwdh: ''
+        };
 
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Gender</Form.Label>
+        this.handleChangeGenderMale = this.handleChangeGenderMale.bind(this);
+        this.handleChangeGenderFemale = this.handleChangeGenderFemale.bind(this);
+        this.handleChangeGenderDivers = this.handleChangeGenderDivers.bind(this);
 
-                <div>
-                    <Form.Check inline label="Male" type="radio" id="male" name="gender" />
-                    <Form.Check inline label="Female" type="radio" id="female" name="gender" />
-                    <Form.Check inline label="Divers" type="radio" id="divers" name="gender" />
-                </div>
-            </Form.Group>
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Birthday</Form.Label>
-                <Form.Control type="date" />
-            </Form.Group>
+    handleChangeGenderMale(value) {
+        this.setState(Object.assign({}, this.state, {gender: 'm'}));
+    }
 
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+    handleChangeGenderFemale(value) {
+        this.setState(Object.assign({}, this.state, {gender: 'f'}));
+    }
 
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" />
-            </Form.Group>
+    handleChangeGenderDivers(value) {
+        this.setState(Object.assign({}, this.state, {gender: 'd'}));
+    }
 
-            <Form.Group controlId="formCreateStudy">
-                <Form.Label>Confirm password</Form.Label>
-                <Form.Control type="password" />
-            </Form.Group>
+    handleSubmit(event) {
+        event.preventDefault();
 
-            <div>
-                By signing up, you agree to our Terms, Privacy Policy, and Cookie Use.
-            </div>
+        let user = {
+            firstName: ReactDOM.findDOMNode(this.firstName).value,
+            lastName: ReactDOM.findDOMNode(this.lastName).value,
+            DoB: ReactDOM.findDOMNode(this.birthday).value,
+            gender: this.state.gender,
+            email: ReactDOM.findDOMNode(this.email).value,
+            password: ReactDOM.findDOMNode(this.password).value,
+            passwordwdh: ReactDOM.findDOMNode(this.passwordwdh).value
+        };
 
-            <Button variant="primary" type="submit">
-                Register
-            </Button>
-        </Form>
-    </Page>
-);
+        this.props.onSubmit(user);
+    }
+
+    render() {
+        return (
+            <Page>
+                <h1>Register</h1>
+                <Form onSubmit={this.handleSubmit} >
+                    <Form.Group controlId="formCreateStudy">
+                        <Form.Label>Firstname</Form.Label>
+                        <Form.Control type="text" placeholder="Enter firstname"
+                                      required={true}
+                                      ref={ firstname => { this.firstName = firstname }} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formCreateStudy">
+                        <Form.Label>Lastname</Form.Label>
+                        <Form.Control type="text" placeholder="Enter lastname"
+                                      required={true}
+                                      ref={ lastname => { this.lastName = lastname }} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formCreateStudy">
+                        <Form.Label>Gender</Form.Label>
+
+                        <div>
+                            <Form.Check inline label="Male" type="radio" id="male" name="gender"
+                                        ref={ gender => { this.genderMale = gender }}
+                                        required={true}
+                                        onChange={this.handleChangeGenderMale} />
+
+                            <Form.Check inline label="Female" type="radio" id="female" name="gender"
+                                        ref={ gender => { this.genderFemale = gender }}
+                                        required={true}
+                                        onChange={this.handleChangeGenderFemale} />
+
+                            <Form.Check inline label="Divers" type="radio" id="divers" name="gender"
+                                        ref={ gender => { this.genderDivers = gender }}
+                                        required={true}
+                                        onChange={this.handleChangeGenderDivers} />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group controlId="formCreateStudy">
+                        <Form.Label>Birthday</Form.Label>
+                        <Form.Control type="date"
+                                      required={true}
+                                      ref={ birthday => { this.birthday = birthday }} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formCreateStudy">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"
+                                      required={true}
+                                      ref={ email => { this.email = email }} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formCreateStudy">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password"
+                                      required={true}
+                                      ref={ password => { this.password = password }} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formCreateStudy">
+                        <Form.Label>Confirm password</Form.Label>
+                        <Form.Control type="password"
+                                      required={true}
+                                      ref={ passwordwdh => { this.passwordwdh = passwordwdh }} />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Check
+                            required
+                            label="By signing up, you agree to our Terms, Privacy Policy, and Cookie Use."
+                            feedback="You must agree before submitting."
+                        />
+                    </Form.Group>
+
+                    <Button variant="primary" id="submit" type="submit">
+                        Register
+                    </Button>
+                    <AlertMessage>{this.props.error ? `${this.props.error}` : ''}</AlertMessage>
+                </Form>
+            </Page>
+        );
+    }
+};
+
+/*
+disabled={
+            this.state.firstname == undefined || this.state.firstname == '' ||
+            this.state.lastname == undefined || this.state.lastname == '' ||
+            this.state.password == undefined || this.state.password == '' ||
+            this.state.email == undefined || this.state.email == '' ||
+            this.state.password == undefined || this.state.password == '' ||
+            this.state.password != this.state.passwordwdh ||
+            this.state.gender == undefined || this.state.gender == ''||
+            this.state.dob == undefined || this.state.dob == ''  ? true : false}
+
+ */
--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -3,16 +3,31 @@
 import React from 'react';
 
 import {Register} from '../components/Register';
+import UserService from "../services/UserService";
+import UserSignup from "../components/movie/UserSignup";
+
 
 export class RegisterView extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {};
+    }
+
+    signup(user) {
+        UserService.register(user.firstName, user.lastName, user.password, user.DoB, user.gender, user.email).then((data) => {
+            this.props.history.push('/');
+        }).catch((e) => {
+            console.error(e);
+            this.setState({
+                error: e
+            });
+        })
     }
 
     render() {
         return (
-            <Register/>
+            <Register onSubmit={(user) => this.signup(user)} error={this.state.error}></Register>
         );
     }
 }
