ffe0bd0d570b49dc97c6b40a97cc25fdd8f383ca
Ludwig Dickmanns <dickmanns@fortiss.org>
2019-07-16 12:20:50 +0200

    StudyFinding:
    * StudyListRow: Redirect implemented

--- src/components/StudyListRow.js
+++ src/components/StudyListRow.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import React from 'react';
-import Button from "react-bootstrap/Button";
+import Button from 'react-bootstrap/Button';
+import Link from 'react-router-dom/Link'
 
 export class StudyListRow extends React.Component {
 
@@ -17,7 +18,7 @@ export class StudyListRow extends React.Component {
                     {this.props.study.street + ' ' + this.props.study.number + ', ' + this.props.study.city + '. ' + this.props.study.country}
                 </td>
                 <td>{this.props.study.rewardAmount + ' ' + this.props.study.rewardType}</td>
-                <td><Button>Details</Button></td>
+                <td><Link to={'studies/application/' + this.props.study.id}><Button>Details</Button></Link></td>
             </tr>
         );
     }
