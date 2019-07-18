09c56228910eb19b27527132e07fe73d3b2118c3
Nico Schreder <nschreder@Nicos-MacBook-Pro.local>
2019-07-19 00:21:16 +0200

    Implement CI for MyStudies view

--- src/components/MyStudies.js
+++ src/components/MyStudies.js
@@ -5,85 +5,78 @@ import React from 'react';
 import Page from './Page'
 import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import Link from "react-router-dom/es/Link";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
-export const MyStudies = () => (
-    <Page>
-        <h1>Study dashboard</h1>
+export class MyStudies extends React.Component {
 
-        <h2>Created studies</h2>
+    render() {
+        return (
+            <Page>
+                <Container>
+                    <h1>Study dashboard</h1>
 
-        <ButtonToolbar>
-            <Link to='/studies/create'><Button>Create study</Button></Link>
-        </ButtonToolbar>
-        <br />
+                    <br />
+                    <h2>My studies</h2>
 
-        <Table bordered hover>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Participants</th>
-                    <th>Payout per participant</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Test 1 djgfjgkfjlkgjdfoiigfscod</td>
-                    <td>20</td>
-                    <td>5.00 €</td>
-                    <td>
-                        <ButtonToolbar>
-                            <Button className="toolbar_btn" href="#">Publish</Button>
-                            <Link to='/studies/manage'><Button className="toolbar_btn">View</Button></Link>
-                            <Button className="toolbar_btn" href="#">Edit</Button>
-                            <Button className="toolbar_btn" href="#">Delete</Button>
-                        </ButtonToolbar>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Test 2 djgfjgkfjlkgjdfoiigfscod</td>
-                    <td>5</td>
-                    <td>15.00 €</td>
-                    <td>
-                        <ButtonToolbar>
-                            <Button className="toolbar_btn" href="#">Publish</Button>
-                            <Link to='/studies/manage'><Button className="toolbar_btn">View</Button></Link>
-                            <Button className="toolbar_btn" href="#">Edit</Button>
-                            <Button className="toolbar_btn" href="#">Delete</Button>
-                        </ButtonToolbar>
-                    </td>
-                </tr>
-            </tbody>
-        </Table>
-        <br />
+                        <Card>
+                            <Card.Header as="h5">
+                                <Row>
+                                    <Col>Electric fans study</Col>
+                                    <Col md="auto"><Button variant="primary" className="input-button">Edit</Button></Col>
+                                    <Col md="auto"><Button variant="primary" className="input-button">Delete</Button></Col>
+                                </Row>
+                            </Card.Header>
+                            <Card.Body>
+                                <Row>
+                                    <p style={{"margin-left": "15px"}}>Experiment on the beneficial effect of electric fans in extreme heat and humidity.</p>
+                                </Row>
+                                <Row>
+                                    <Col md="auto"><img src="https://img.icons8.com/windows/32/000000/conference.png" /></Col>
+                                    <Col>3 / 25</Col>
+                                    <Col md="auto"><img src="https://img.icons8.com/windows/32/000000/money-bag.png" /></Col>
+                                    <Col></Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    <br />
 
-        <h2>Applied studies</h2>
-        <Table bordered hover>
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Organizer</th>
-                <th>Date / Time</th>
-                <th>Payout</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>djgfjgkfjlkgjdfoiigfscod</td>
-                    <td>Nico Schreder</td>
-                    <td>06/12/2019 18:00 - 06/12/2019 19:00</td>
-                    <td>5.00 €</td>
-                    <td>
-                        <ButtonToolbar>
-                            <Button className="toolbar_btn" href="#">View</Button>
-                            <Button className="toolbar_btn" href="#">Cancel</Button>
-                        </ButtonToolbar>
-                    </td>
-                </tr>
-            </tbody>
-        </Table>
-    </Page>
-);
+                    <ButtonToolbar>
+                        <Link to='/studies/create'><Button className="input-button">Create new study</Button></Link>
+                    </ButtonToolbar>
+
+                    <br/>
+
+                    <h2>Applied studies</h2>
+
+                    <Card>
+                        <Card.Header as="h5">
+                            <Row>
+                                <Col>Electric fans study</Col>
+                                <Col md="auto"><Button variant="primary" className="input-button">Check in</Button></Col>
+                                <Col md="auto"><Button variant="primary" className="input-button">Cancel</Button></Col>
+                            </Row>
+                        </Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <p style={{"margin-left": "15px"}}>Experiment on the beneficial effect of electric fans in extreme heat and humidity.</p>
+                            </Row>
+                            <Row>
+                                <Col md="auto"><img src="https://img.icons8.com/windows/32/000000/address.png" /></Col>
+                                <Col></Col>
+                                <Col md="auto"><img src="https://img.icons8.com/windows/32/000000/time.png" /></Col>
+                                <Col>30 min</Col>
+                                <Col md="auto"><img src="https://img.icons8.com/windows/32/000000/money-bag.png" /></Col>
+                                <Col></Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Container>
+            </Page>
+        )
+    }
+}
